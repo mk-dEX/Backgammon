e79afbdbd442e177fb3688639765442a82eaa8e4
Nils Plaschke <contact@nils-canada.de>
2012-03-14 15:39:11 +0100

    added dice and chip event

--- src/backgammon/event/BackgammonEvent.java
+++ src/backgammon/event/BackgammonEvent.java
@@ -1,5 +1,23 @@
 package backgammon.event;
 
-public class BackgammonEvent {
+class BackgammonEvent {
 
+	/**
+	 * Beschreibt ein Event, der beschreibt 
+	 * dass ein Spielstein gesetzt wurde
+	 */
+	public BackgammonEvent(int listentry, Object pl)
+	{
+		
+	}
+	
+	/**
+	 * Beschreibt ein Event, der beschreibt 
+	 * dass gewürfelt wurde
+	 */
+	public BackgammonEvent(Object dice ,Object pl)
+	{
+		
+	}
+	
 }
