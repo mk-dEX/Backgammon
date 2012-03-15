f5298eff90b1bc4ee0c49bd69b89d97f7295e34a
Nils Plaschke <contact@nils-canada.de>
2012-03-15 22:06:13 +0100

    added Style interface added standard style class added drawing functions

--- src/backgammon/view/BackgammonViewGUI.java
+++ src/backgammon/view/BackgammonViewGUI.java
@@ -1,5 +1,47 @@
 package backgammon.view;
 
+import backgammon.view.styles.Standard;
+
 public class BackgammonViewGUI {
+	
+	/**
+	 * Represents the actual style of the game
+	 */
+	private Style style;
+
+	/**
+	 * Normal Constructor, if no style is choosed, the default one will be taken.
+	 */
+	public BackgammonViewGUI() {
+		super();
+		this.style = new Standard();
+	}
+	
+	/**
+	 * Normal Constructor with specific style to use
+	 * 
+	 * @param style The Style for Board and Checkers
+	 */
+	public BackgammonViewGUI(Style style) {
+		super();
+		this.style = style;
+	}
+	
+	/**
+	 * 
+	 * @return Style actual Board style
+	 */
+	public Style getStyle() {
+		return style;
+	}
+
+	/**
+	 * 
+	 * @param style the style the board should use from now on.
+	 */
+	public void setStyle(Style style) {
+		this.style = style;
+	}
+	
 
 }
