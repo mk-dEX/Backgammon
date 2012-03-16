39c9ec237209329511182865b8e3b79ce56cf3e3
Nils Plaschke <contact@nils-canada.de>
2012-03-16 20:14:03 +0100

    removed watermark

--- src/backgammon/view/BackgammonViewSettings.java
+++ src/backgammon/view/BackgammonViewSettings.java
@@ -280,7 +280,7 @@ public class BackgammonViewSettings extends JFrame {
 				// JFormDesigner evaluation mark
 				panel1.setBorder(new javax.swing.border.CompoundBorder(
 					new javax.swing.border.TitledBorder(new javax.swing.border.EmptyBorder(0, 0, 0, 0),
-						"JFormDesigner Evaluation", javax.swing.border.TitledBorder.CENTER,
+						"", javax.swing.border.TitledBorder.CENTER,
 						javax.swing.border.TitledBorder.BOTTOM, new java.awt.Font("Dialog", java.awt.Font.BOLD, 12),
 						java.awt.Color.red), panel1.getBorder())); panel1.addPropertyChangeListener(new java.beans.PropertyChangeListener(){public void propertyChange(java.beans.PropertyChangeEvent e){if("border".equals(e.getPropertyName()))throw new RuntimeException();}});
 
