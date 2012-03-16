0ca8446b270d4fe6c874f4b20038ce6ce7a1d9f8
Nils Plaschke <contact@nils-canada.de>
2012-03-16 10:33:42 +0100

    updated GUI to match the class diagram

--- src/backgammon/view/BackgammonViewGUI.java
+++ src/backgammon/view/BackgammonViewGUI.java
@@ -1,47 +1,105 @@
 package backgammon.view;
 
-import backgammon.view.styles.Standard;
+import backgammon.controller.IControllerDelegate;
 
 public class BackgammonViewGUI {
 	
 	/**
-	 * Represents the actual style of the game
+	 * Represents the controller
 	 */
-	private Style style;
-
-	/**
-	 * Normal Constructor, if no style is choosed, the default one will be taken.
-	 */
-	public BackgammonViewGUI() {
-		super();
-		this.style = new Standard();
-	}
+	private IControllerDelegate controller;
 	
 	/**
 	 * Normal Constructor with specific style to use
 	 * 
 	 * @param style The Style for Board and Checkers
 	 */
-	public BackgammonViewGUI(Style style) {
-		super();
-		this.style = style;
+	public BackgammonViewGUI(IControllerDelegate controller) {
+		
+		this.controller = controller;
+		//TODO: Register Listener on Model.
+		
+	}
+	
+	
+	/**
+	 * Draws a Checker in the specific prime
+	 * @param prime Represents the prime as integer.
+	 * @return Boolean Indicates whether the drawing was successful or not.
+	 */
+	private boolean drawChecker(int prime)
+	{
+		return false;
 	}
 	
 	/**
 	 * 
-	 * @return Style actual Board style
+	 * This function draws a checker from one prime to another.
+	 * The function will NOT check if there are other checkers at the stopping prime (opponents one)
+	 * and will take the first checker from the starting prime.
+	 * 
+	 * @param from The starting prime as integer.
+	 * @param to The prime the checker should be moved to.
+	 * @return Boolean Inidcates whether the move has been drawn successfully or not
 	 */
-	public Style getStyle() {
-		return style;
+	private boolean moveChecker(int from, int to)
+	{
+		return false;
+	}
+	
+	/**
+	 * This function will render ONLY the Board on the internal Panel
+	 * 
+	 * @return Boolean whether the Board has been drawn successfully or not
+	 */
+	private boolean drawBoard()
+	{
+		return false;
+	}
+	
+	/**
+	 * This function will draw the normal dices on the board.
+	 * 
+	 * @param first The value of the first dice
+	 * @param second The value of the second dice
+	 * @param player indicates the player side, if Zero (0) it will draw the first on the one and the second on the other side.
+	 * @return Boolean whether the draw was succeeded or not
+	 */
+	private boolean drawDices(int first, int second, int player)
+	{
+		return false;
+	}
+	
+	/**
+	 * This function will draw the Doubledice on the board
+	 * 
+	 * @param value The value of the dice
+	 * @param player The side the dice should be drawn on, Zero (0) means in the middle
+	 * @return Boolean whether the draw was succeeded or not
+	 */
+	private boolean drawDoubleDice(int value, int player)
+	{
+		return false;
+	}
+	
+	/**
+	 * This function will draw all information needed, such as player names, pipcount for both players and the actual winning points.
+	 * @param game Game Object that contains all information needed.
+	 * @return Boolean whether the draw was succeeded or not
+	 */
+	private boolean drawInformation(Object game)
+	{
+		return false;
 	}
 
 	/**
 	 * 
-	 * @param style the style the board should use from now on.
+	 * Testfunction
+	 * 
+	 * @return Boolean True or false
 	 */
-	public void setStyle(Style style) {
-		this.style = style;
+	public boolean makeTestDraw() {
+		return false;
 	}
-	
 
 }
