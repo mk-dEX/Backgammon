54558930dfa662ec5ad2895805b62d0c7ccec636
Marc Kirchmann <marc.kirchmann@gmail.com>
2012-03-16 15:17:01 +0100

    Added javadoc comments

--- src/backgammon/app/GameAgent.java
+++ src/backgammon/app/GameAgent.java
@@ -6,16 +6,26 @@ import backgammon.controller.ControllerDelegate;
 
 public class GameAgent {
 	
+	/**
+	 * Calls startUpdateSettings
+	 */
 	public GameAgent() {
 		
 		startUpdateSettings();
 	}
 	
+	/**
+	 * Shows {@link BackgammonViewSettings}.
+	 */
 	public void startUpdateSettings() {
 		
 		new BackgammonViewSettings(this);
 	}
 	
+	/**
+	 * Creates new ControllerDelegate instance to start a new game.
+	 * @param newSettings The {@link GameSettings} initialized by the {@link BackgammonViewSettings}Settings class
+	 */
 	public void updateSettings(GameSettings newSettings) {
 		
 		new ControllerDelegate(this, newSettings);
