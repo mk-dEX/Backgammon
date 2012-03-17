0900ad826f461b1ec7ececdc4c95242f2b487edd
Marc Kirchmann <marc.kirchmann@gmail.com>
2012-03-17 14:56:55 +0100

    startGame()-method added to IDataModel interface

--- src/backgammon/model/IDataModel.java
+++ src/backgammon/model/IDataModel.java
@@ -1,8 +1,9 @@
 package backgammon.model;
 
+import backgammon.app.GameSettings;
 import backgammon.listener.IModelEventListener;
 
 public interface IDataModel {
-	
 	public void addDataModelListener(IModelEventListener listener);
+	public void startGame();
 }
