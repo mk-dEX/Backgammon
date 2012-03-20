1a4d6fff1f72cd7aea466bde6eb5a382b329adb9
Marc Kirchmann <marc.kirchmann@gmail.com>
2012-03-20 17:56:33 +0100

    a bar can neither be full, nor blocked by another player. class Out adopts those methods

--- src/backgammon/model/board/Bar.java
+++ src/backgammon/model/board/Bar.java
@@ -1,8 +1,14 @@
 package backgammon.model.board;
 
+import backgammon.model.interfaces.IPlayer;
+
 public class Bar extends Point {
 
 	public boolean isFull() {
+		return false;
+	}
+	
+	public boolean isBlockedByPlayer(IPlayer player) {
 		return false;
 	}
 	
