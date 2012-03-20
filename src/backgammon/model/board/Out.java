9d2a76c913306a7c985092114fc3d988753bdc82
Marc Kirchmann <marc.kirchmann@gmail.com>
2012-03-20 17:55:18 +0100

    removeChecker does only care if Checker vector is empty. If not, a Checker is removed.

--- src/backgammon/model/board/Out.java
+++ src/backgammon/model/board/Out.java
@@ -1,6 +1,18 @@
 package backgammon.model.board;
 
+import backgammon.model.interfaces.IPlayer;
+
 public class Out extends Bar {
 
+	public boolean removeChecker(IPlayer player) {
+		
+		if (this.isEmpty()) {
+			return false;
+		}
+		
+		this.checkers.remove(this.checkers.size() - 1);
+		
+		return true;
+	}
 	
 }
