a9875b518780f91956be80c182fc544442a73bb4
Marc Kirchmann <marc.kirchmann@gmail.com>
2012-03-20 17:22:52 +0100

    added constructor

---
+++ src/backgammon/model/player/ComputerPlayer.java
@@ -0,0 +1,16 @@
+package backgammon.model.player;
+
+import backgammon.app.GameSettings;
+import backgammon.model.interfaces.IDataController;
+import backgammon.model.interfaces.IPlayer;
+
+public class ComputerPlayer extends HumanPlayer implements IPlayer {
+
+	private GameSettings.KIMode mode;
+	
+	public ComputerPlayer(String playerName, IDataController rootDataController, GameSettings.KIMode mode) {
+		super(playerName, rootDataController);
+		this.mode = mode;
+	}
+	
+}
