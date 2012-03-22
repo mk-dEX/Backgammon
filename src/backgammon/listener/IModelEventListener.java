cef7a67f84783349640168b6555e3b76be224528
Marc Kirchmann <marc.kirchmann@gmail.com>
2012-03-22 17:56:41 +0100

    updated interface

--- src/backgammon/listener/IModelEventListener.java
+++ src/backgammon/listener/IModelEventListener.java
@@ -1,5 +1,38 @@
 package backgammon.listener;
 
+import backgammon.event.CheckerMoveEvent;
+import backgammon.event.DiceEvent;
+import backgammon.event.InfoEvent;
+import backgammon.event.PlayerMoveRequest;
+
 public interface IModelEventListener {
 
+	/**
+	 * A checker has to be added to/removed from the board.
+	 * @param event The corresponding {@link CheckerMoveEvent}
+	 * @return >=0 if the event has been handled correctly
+	 */
+	public int handleCheckerMoveEvent(CheckerMoveEvent event);
+	
+	/**
+	 * New dice data is available.
+	 * @param event The corresponding {@link DiceEvent}
+	 * @return >=0 if the event has been handled correctly
+	 */
+	public int handleDiceEvent(DiceEvent event);
+	
+	/**
+	 * An info string is available.
+	 * @param event The corresponding {@link InfoEvent}
+	 * @return >=0 if the event has been handled correctly
+	 */
+	public int handleInfoEvent(InfoEvent event);
+	
+	/**
+	 * Checkers of one player have to be moved using the GUI. 
+	 * @param request The corresponding {@link PlayerMoveRequest}
+	 * @return >=0 if the event has been handled correctly
+	 */
+	public int handlePlayerMoveRequest(PlayerMoveRequest request);
+	
 }
