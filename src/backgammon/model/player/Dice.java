e261188a5460e93c36d2980910faf6e7c4f05ae6
Marc Kirchmann <marc.kirchmann@gmail.com>
2012-03-22 17:58:57 +0100

    one simplier method

--- src/backgammon/model/player/Dice.java
+++ src/backgammon/model/player/Dice.java
@@ -5,20 +5,15 @@ import java.util.Random;
 
 public class Dice {
 	
-	// Standard WŸrfel
-	private int min = 1;
-	private int max = 6;
-	
-	public void setRange(int min, int max) {
-		if (min < max && min >= 0 && max >= 0) {
-			this.min = min;
-			this.max = max;
-		}
-	}
-	
-	public int roll() {
+	public int roll(int min, int max) {
+		
 		Random generator = new Random(Calendar.getInstance().getTimeInMillis());
-		return generator.nextInt(this.max) + this.min;
+		
+		if (min < max && min >= 0 && max >= 0) {
+			return generator.nextInt(max) + min;
+		} else {
+			return generator.nextInt(6) + 1;
+		}
 	}
 	
 }
