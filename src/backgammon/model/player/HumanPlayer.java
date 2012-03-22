04bae57f520d8c4287263c35b0991d9bdcd832d7
Marc Kirchmann <marc.kirchmann@gmail.com>
2012-03-22 17:59:33 +0100

    beginning of move implementation

--- src/backgammon/model/player/HumanPlayer.java
+++ src/backgammon/model/player/HumanPlayer.java
@@ -7,14 +7,30 @@ public class HumanPlayer implements IPlayer {
 
 	protected String name;
 	protected IDataController rootDataController;
+	protected Dice dice;
 	
 	public HumanPlayer(String playerName, IDataController rootDataController) {
 		this.name = playerName;
 		this.rootDataController = rootDataController;
+		this.dice = new Dice();
+	}
+	
+	public int rollDice(int min, int max) {
+		return this.dice.roll(min, max);
 	}
 
 	public void init() {
 		// set init chips onto board
-	}
 
+	}
+	
+	public Move move() {
+		
+		// EVENT GETHUMANMOVE(int playerID)
+		
+		
+		return null;
+	}
+	
+	
 }
