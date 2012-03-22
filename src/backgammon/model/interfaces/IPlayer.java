26443a94e859ab0c66420b7d904a5efb09c3054d
Marc Kirchmann <marc.kirchmann@gmail.com>
2012-03-22 17:58:28 +0100

    rollDice with range added

--- src/backgammon/model/interfaces/IPlayer.java
+++ src/backgammon/model/interfaces/IPlayer.java
@@ -1,7 +1,11 @@
 package backgammon.model.interfaces;
 
+import backgammon.model.player.Move;
+
 public interface IPlayer {
 
 	public void init();
+	public int rollDice(int min, int max);
+	public Move move();
 	
 }
