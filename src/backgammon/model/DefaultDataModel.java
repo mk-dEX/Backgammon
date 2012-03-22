88df05937517e6d928367ffba0acf2a531149a02
Marc Kirchmann <marc.kirchmann@gmail.com>
2012-03-22 18:20:35 +0100

    null pointer exception fixed

--- src/backgammon/model/DefaultDataModel.java
+++ src/backgammon/model/DefaultDataModel.java
@@ -22,15 +22,22 @@ public class DefaultDataModel implements IDataController {
 	public DefaultDataModel(GameSettings currentSettings) {
 		this.settings = currentSettings;
 		this.gameBoard = new DefaultBackgammonBoard();
-		initPlayer(player1, this.settings.getNamePlayer1(), this.settings.getSelectedKIModePlayer1());
-		initPlayer(player2, this.settings.getNamePlayer2(), this.settings.getSelectedKIModePlayer2());
-	}
-	
-	private void initPlayer(IPlayer player, String name, GameSettings.KIMode mode) {
-		if (mode.equals(GameSettings.KIMode.HUMAN)) {
-			player = new HumanPlayer(name, this);
+		
+		GameSettings.KIMode KIModePlayer1 = this.settings.getSelectedKIModePlayer1();
+		GameSettings.KIMode KIModePlayer2 = this.settings.getSelectedKIModePlayer2();
+		String namePlayer1 = this.settings.getNamePlayer1();
+		String namePlayer2 = this.settings.getNamePlayer2();
+		
+		if (KIModePlayer1.equals(GameSettings.KIMode.HUMAN)) {
+			player1 = new HumanPlayer(namePlayer1, this);
 		} else {
-			player = new ComputerPlayer(name, this, mode);
+			player1 = new ComputerPlayer(namePlayer1, this, KIModePlayer1);
+		}
+		
+		if (KIModePlayer2.equals(GameSettings.KIMode.HUMAN)) {
+			player2 = new HumanPlayer(namePlayer2, this);
+		} else {
+			player2 = new ComputerPlayer(namePlayer2, this, KIModePlayer2);
 		}
 	}
 		
