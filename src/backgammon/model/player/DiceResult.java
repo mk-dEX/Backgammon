4193096840db41b5319b9a6224c32d13d620c86f
mk-dEX <marc.kirchmann@gmail.com>
2012-03-29 19:58:54 +0200

    new class extending old Vector<Integer>. adds functionality

---
+++ src/backgammon/model/player/DiceResult.java
@@ -0,0 +1,14 @@
+package backgammon.model.player;
+
+import java.util.Vector;
+
+public class DiceResult extends Vector<Integer> {
+	private static final long serialVersionUID = 1L;
+	
+	public DiceResult() {
+
+	}
+	
+	
+	
+}
