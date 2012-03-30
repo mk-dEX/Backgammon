0cbe4ff01d7ef7148d42600eea5c8759b7f2c46e
mk-dEX <marc.kirchmann@gmail.com>
2012-03-30 11:07:33 +0200

    controller delegate re-added

--- src/backgammon/model/interfaces/IDataModel.java
+++ src/backgammon/model/interfaces/IDataModel.java
@@ -4,8 +4,10 @@ import backgammon.listener.IModelEventListener;
 
 public interface IDataModel {
 	public void addDataModelListener(IModelEventListener listener);
+	
 	public void initGameCheckers();
 	public void initGame();
+	
 	public void initNextPlayerMove();
-
+	public boolean startMove(int playerID);
 }
