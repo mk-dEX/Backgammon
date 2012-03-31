7dfc00f061aae2b80bce0e28d0a9aa3455bd7149
mk-dEX <marc.kirchmann@gmail.com>
2012-03-31 14:26:06 +0200

    the current diceResult of a player is stored in the player object

--- src/backgammon/model/player/Player.java
+++ src/backgammon/model/player/Player.java
@@ -7,10 +7,20 @@ public abstract class Player {
 	protected String name;
 	protected IDataController rootDataController;
 	protected Dice dice;
+	protected DiceResult currentDiceResult;
 	
 	public int rollDice(int min, int max) {
 		return this.dice.roll(min, max);
 	}
+	
+	public void setCurrentDiceResult(DiceResult newDiceResult) {
+		this.currentDiceResult = newDiceResult;
+	}
+	
+	public DiceResult getCurrentDiceResult() {
+		return this.currentDiceResult;
+	}
+	
 	public abstract boolean isHuman();
 	
 }
