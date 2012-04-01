b80028121d58d7526966c349865f9e1ddf12a14f
mk-dEX <marc.kirchmann@gmail.com>
2012-04-01 11:24:52 +0200

    added getCheckerListForIndex to interface

--- src/backgammon/model/interfaces/IDataController.java
+++ src/backgammon/model/interfaces/IDataController.java
@@ -3,4 +3,5 @@ package backgammon.model.interfaces;
 public interface IDataController extends IDataModel {
 	
 	public IBackgammonBoard getBackgammonBoard();
+	public ICheckerList getCheckerListForIndex(int index) throws Exception;
 }
