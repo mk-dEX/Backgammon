ebd65c7c314f704679e6f23a1fbfbd7b09a226ba
Marc Kirchmann <marc.kirchmann@gmail.com>
2012-05-29 19:10:27 +0200

    comments IModelEventListener

--- src/backgammon/listener/IModelEventListener.java
+++ src/backgammon/listener/IModelEventListener.java
@@ -2,7 +2,14 @@ package backgammon.listener;
 
 import backgammon.event.BackgammonEvent;
 
+/**
+ * Interface für View-Klassen, die Events aus dem Datenmodell verarbeiten und visualisieren
+ */
 public interface IModelEventListener {
 
+	/**
+	 * Behandelt ein allgemeines {@link BackgammonEvent}
+	 * @param event Das {@link BackgammonEvent}
+	 */
 	public void handleBackgammonEvent(BackgammonEvent event);
 }
